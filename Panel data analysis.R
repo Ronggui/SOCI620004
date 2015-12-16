c4a5abd017c2c774b6ddf4ee6e95642eb2b663c7
Ronggui <ronggui.huang@gmail.com>
2015-12-16 23:10:20 +0800

    pannel data analysis with R

---
+++ Panel data analysis.R
@@ -0,0 +1,23 @@
+library(plm)
+
+data("Produc", package = "plm")
+
+## pooling
+pool.mod <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, data = Produc, model = "pooling", index = c("state","year"))
+summary(pool.mod)
+
+##  first-differences
+fd.mod <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, data = Produc, model = "fd", index = c("state","year"))
+summary(fd.mod)
+
+## fixed effect
+fixed.mod <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, data = Produc, index = c("state","year"))
+summary(fixed.mod)
+
+## randome effect
+rand.mod <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, data = Produc, model = "random", index = c("state","year"))
+summary(rand.mod)
+
+## hausman test
+phtest(fixed.mod, rand.mod)
+## if significant, fixed effect model is preferred
